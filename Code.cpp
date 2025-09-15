df05c4a6f2796e3c5a579ddfd8383d85428b7c9a
Ayesha Kamran <ayesha0ktk@gmail.com>
2025-09-16 01:27:16 +0500

    Task 1

--- Code.cpp
+++ Code.cpp
@@ -1,6 +1,24 @@
 #include<iostream>
 using namespace std;
 int main(){
-    cout<<"Hello";
+    int arr[50];
+    cout<<"Enter the elements of the array enter -1 to stop"<<endl;
+    int n=0;
+    for(int i=0;i<50;i++){
+        cin>>arr[i];
+        n++;
+        if(arr[i]==-1){
+            break;
+        }
+    }
+    int key;
+    cout<<"Enter the element you want to find."<<endl;
+    cin>>key;
+    cout<<"This element is present at the following indices:"<<endl;
+    for(int i=0;i<n;i++){
+        if(arr[i]==key){
+            cout<<i<<" ";
+        }
+    }
     return 0;
 }
